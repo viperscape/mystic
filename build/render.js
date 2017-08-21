989ad83e35a9d34ca6fb189bf5d5ac2dc7c5eb19
viperscape <chrisfgill@gmail.com>
2017-08-21 16:49:09 -0400

    update with touch

--- build/render.js
+++ build/render.js
@@ -9,17 +9,10 @@ function render() {
 exports.render = render;
 render();
 function draw_test(ctx) {
-    var g = new pixi.Graphics();
-    // set a fill and line style
-    g.beginFill(0xFF3300);
-    g.lineStyle(10, 0xffd900, 1);
-    // draw a shape
-    g.moveTo(50, 50);
-    g.lineTo(250, 50);
-    g.lineTo(100, 100);
-    g.lineTo(250, 220);
-    g.lineTo(50, 220);
-    g.lineTo(50, 50);
-    g.endFill();
-    ctx.stage.addChild(g);
+    var text = new pixi.Text('draw test', { fontFamily: 'Arial', fontSize: 24, fill: 0xff1010, align: 'center' });
+    text.interactive = true;
+    text.on('mousedown', function (event) {
+        console.log(event);
+    });
+    ctx.stage.addChild(text);
 }
--- render.ts
+++ render.ts
@@ -10,19 +10,10 @@ export function render () {
 render()
 
 function draw_test(ctx) {
-    let g = new pixi.Graphics();
-    // set a fill and line style
-    g.beginFill(0xFF3300);
-    g.lineStyle(10, 0xffd900, 1);
-
-    // draw a shape
-    g.moveTo(50,50);
-    g.lineTo(250, 50);
-    g.lineTo(100, 100);
-    g.lineTo(250, 220);
-    g.lineTo(50, 220);
-    g.lineTo(50, 50);
-    g.endFill();
-
-    ctx.stage.addChild(g);
+    let text = new pixi.Text('draw test',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
+    text.interactive = true;
+    text.on('mousedown', (event) => {
+        console.log(event)
+     });
+    ctx.stage.addChild(text);
 }
