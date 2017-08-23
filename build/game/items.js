d07060d663929e82dcd2be5935d08ebd2643c9dc
viperscape <chrisfgill@gmail.com>
2017-08-23 12:24:55 -0400

    parse potion items

--- build/game/items.js
+++ build/game/items.js
@@ -1,12 +1,45 @@
 "use strict";
 exports.__esModule = true;
-var items = require("../assets/items.json");
+var items_ = require("../assets/items.json");
+// NOTE: from methods transform a generic Object parsed from JSON into said class
 function load() {
-    return items;
+    var i = new Items;
+    i.from(items_);
+    return i;
 }
 exports.load = load;
-var Potion = (function () {
-    function Potion() {
+var Items = /** @class */ (function () {
+    function Items() {
+        this.potions = [];
     }
+    Items.prototype.from = function (obj) {
+        var _this = this;
+        obj["potions"].forEach(function (element) {
+            var p = new Potion;
+            p.from(element);
+            _this.potions.push(p);
+        });
+    };
+    return Items;
+}());
+var Potion = /** @class */ (function () {
+    function Potion() {
+        this.name = "unknown potion";
+        this.kind = "unknown kind";
+        this.strength = 0;
+        this.stamina = 0;
+        this.health = 0;
+        this.concentration = 0;
+        this.insight = 0;
+    }
+    Potion.prototype.from = function (obj) {
+        this.name = obj["name"];
+        this.kind = obj["kind"];
+        this.strength = obj["strength"];
+        this.stamina = obj["stamia"];
+        this.health = obj["health"];
+        this.concentration = obj["concentration"];
+        this.insight = obj["insight"];
+    };
     return Potion;
 }());
--- game/items.ts
+++ game/items.ts
@@ -1,15 +1,59 @@
-const items = require("../assets/items.json");
+const items_ = require("../assets/items.json");
 
-export function load(): any {
-    return items;
+// NOTE: from methods transform a generic Object parsed from JSON into said class
+
+export function load(): Items {
+    let i = new Items;
+    i.from(items_);
+    return i;
+}
+
+class Items {
+    potions: Potion[];
+
+    constructor () {
+        this.potions = [];
+    }
+
+    from(obj:Object) {
+        obj["potions"].forEach(element => {
+            let p = new Potion;
+            p.from(element);
+            this.potions.push(p);
+        });
+    }
 }
 
 class Potion {
     name: string;
     kind: string;
+    // TODO: add a description field?
 
     //modifiers
     strength: number;
     stamina: number;
     health: number;
+    concentration: number;
+    insight: number;
+
+    constructor () { // set defaults, makes applying these later a cinch
+        this.name = "unknown potion";
+        this.kind = "unknown kind";
+        this.strength = 0;
+        this.stamina = 0;
+        this.health = 0;
+        this.concentration = 0;
+        this.insight = 0;
+    }
+
+    from(obj: Object) {
+        this.name = obj["name"];
+        this.kind = obj["kind"];
+
+        this.strength = obj["strength"];
+        this.stamina = obj["stamia"];
+        this.health = obj["health"];
+        this.concentration = obj["concentration"];
+        this.insight = obj["insight"];
+    }
 }
