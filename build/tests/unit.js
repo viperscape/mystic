5e670c2a52433592b08db3dd209665409458be18
viperscape <chrisfgill@gmail.com>
2017-08-24 15:10:08 -0400

    remove build info

--- build/tests/unit.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var tests_1 = require("./tests");
-var potion_1 = require("./potion");
-var tests = [new potion_1.PotionTests];
-var _ = new tests_1.Tests(tests);
