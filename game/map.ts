361345f3758d222195ea55e5bdc4f9f70210a7d7
viperscape <chrisfgill@gmail.com>
2017-08-29 16:39:24 -0400

    mat colors

--- game/map.ts
+++ game/map.ts
@@ -23,8 +23,15 @@ export class Map {
     render (r:Renderer) {
         this.layout.forEach((row, ridx) => {
             row.forEach((e, eidx) => {
+                let mat = { color: 0x0, opacity: 1 };
+                let stone_brown = 0x4b331d;
+                let stone_grey = 0x424242;
+
+                if (e["tile"] == "stone-grey") mat.color = stone_grey;
+                else mat.color = stone_brown;
+
                 var geometry = new Three.BoxGeometry(1,0,1);
-                var material = new Three.MeshNormalMaterial();
+                var material = new Three.MeshBasicMaterial(mat);
                 var cube = new Three.Mesh(geometry, material);
                 r.scene.add(cube);
                 cube.position.x = eidx;
