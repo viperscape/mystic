dc08d675ae4cd9a9fd50ee4a99165f0da3cb03bf
viperscape <chrisfgill@gmail.com>
2017-08-29 15:42:57 -0400

    test map loading

--- tests/unit.ts
+++ tests/unit.ts
@@ -1,6 +1,7 @@
 import {Tests} from "./tests";
 
 import {PotionTests} from "./potion";
+import {MapTests} from "./map";
 
-let tests = [new PotionTests];
+let tests = [new PotionTests, new MapTests];
 let _ = new Tests(tests);
