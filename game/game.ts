e527739954e8d7a521227a6806193b1ca33b971d
viperscape <chrisfgill@gmail.com>
2017-09-07 16:43:57 -0400

    hittests

--- game/game.ts
+++ game/game.ts
@@ -3,6 +3,7 @@ import items = require("./items");
 import {Player} from "./player";
 import {Map} from "./map";
 import {Move} from "./move";
+import Three = require("three");
 
 export function run(target_gui) {
     //render.render(target_gui);
@@ -13,15 +14,28 @@ export function run(target_gui) {
 
         let move = new Move(map.player.position, [0,3], map);
         
-        check_input();
+        check_input(renderer, map);
         check_resize(renderer);
     });
 }
 
-function check_input () {
+function check_input (r: render.Renderer, map: Map) {
     document.addEventListener('mousedown', onMouseDown, false);
-    function onMouseDown(e) {
-        console.log(e);
+    function onMouseDown(event) {
+        //event.preventDefault(); // ??
+        let raycaster = new Three.Raycaster();
+        let mouse = new Three.Vector2();
+
+        mouse.x = ( event.clientX / r.ctx.domElement.clientWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / r.ctx.domElement.clientHeight ) * 2 + 1;
+        raycaster.setFromCamera( mouse, r.camera );
+        let objects: Three.Mesh[] = [];
+        map.objects.forEach((e) => { objects.push(e.renderable.mesh) }); // TODO: track tiles, player, and items separately
+
+        let intersects = raycaster.intersectObjects( objects );
+        if ( intersects.length > 0 ) {
+            console.log("hit!");
+        }
     }
 }
 
