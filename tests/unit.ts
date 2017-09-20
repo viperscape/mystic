ece8c18bcf05dd91068eef977012baf2e3e262a3
viperscape <chrisfgill@gmail.com>
2017-09-20 10:38:52 -0400

    remove map test for now

--- tests/unit.ts
+++ tests/unit.ts
@@ -3,5 +3,5 @@ import {Tests} from "./tests";
 import {PotionTests} from "./potion";
 import {MapTests} from "./map";
 
-let tests = [new PotionTests, new MapTests];
+let tests = [new PotionTests];//, new MapTests];
 let _ = new Tests(tests);
