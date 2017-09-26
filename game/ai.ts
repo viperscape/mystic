841a52587b9f552e98f624ba815c3a3031451547
viperscape <chrisfgill@gmail.com>
2017-09-26 15:57:46 -0400

    add some basic combat ai to figure things out with

--- game/ai.ts
+++ game/ai.ts
@@ -1,28 +1,66 @@
-export class AI {
-    influencers: Influencer[]
+import {Attributes} from "./attr";
 
-    step(): AI {
+export class AI {
+    protected influencers: {string:Influencer};
+
+    constructor (is?: {string:Influencer}) {
+        if (is) this.influencers = is;
+    }
+
+    step() {
         for (var i in this.influencers) {
-            let result = this.influencers[i].process();
-            if (result) return result;
+            this.influencers[i].process();
         }
     }
+
+    insert (name: string, i:Influencer) {
+        this.influencers[name] = i;
+    }
+
+    remove (name:string) { delete this.influencers[name] }
 }
 
 export class Influencer {
     fn: () => number;
     total: number;
-    next: AI;
+    next: () => number;
 
-    constructor (next: AI, fn: () => number) {
+    constructor (next: () => number, fn: () => number) {
         this.fn = fn;
-        this.total = 0;
+        this.total = 0.5;
         this.next = next;
     }
-    process(): AI { 
+    process() { 
         this.total += this.fn(); 
         if (this.total > 1) {
-            return this.next;
+            this.fn = this.next;
         }
      }
+}
+
+export class CombatAI {
+    state: (() => void)[];
+    observe: Attributes;
+
+    constructor (attr: Attributes) {
+        this.state = [this.patrol];
+        this.observe = attr;
+    }
+
+    process() {
+        let last = this.state[this.state.length];
+        if (this.observe.health < 20) {
+            if (last != this.flee) this.state.push(this.flee);
+        }
+        else if (!last) this.state.push(this.patrol);
+
+        this.state[this.state.length]();
+    }
+
+    flee () {
+        console.log("flee");
+        if (this.observe.health > 25) this.state.pop();
+    }
+    attack () { console.log("attack") }
+    patrol () { console.log("patrol") }
 }
